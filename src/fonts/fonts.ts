2689459243c4419e61126309cffd2035cae61dbc
Maksym Puzanov <puzanov.maksym@gmail.com>
2024-02-26 19:31:50 +0200

    add fonts

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -1,19 +1,19 @@
 import { Roboto, Open_Sans, Poppins, Nunito } from 'next/font/google'
 // import { Poppins } from 'next/font/google'
 
-// export const roboto = Roboto({
-// 	weight: ['400', '500'],
-// 	subsets: ['cyrillic', 'latin'],
-// 	variable: '--roboto-font',
-// 	display: 'swap'
-// })
+export const roboto = Roboto({
+	weight: ['400', '500'],
+	subsets: ['cyrillic', 'latin'],
+	variable: '--roboto-font',
+	display: 'swap'
+})
 
-// export const open_sans = Open_Sans({
-// 	weight: ['400', '600'],
-// 	subsets: ['cyrillic', 'latin'],
-// 	variable: '--font-inter',
-// 	display: 'swap'
-// })
+export const open_sans = Open_Sans({
+	weight: ['400', '600'],
+	subsets: ['cyrillic', 'latin'],
+	variable: '--font-inter',
+	display: 'swap'
+})
 
 export const poppins = Poppins({
 	weight: ['400', '500', '700'],
@@ -22,9 +22,9 @@ export const poppins = Poppins({
 	display: 'swap'
 })
 
-// export const nunito = Nunito({
-// 	weight: ['500', '700'],
-// 	subsets: ['latin', 'cyrillic'],
-// 	variable: '--font-poppins',
-// 	display: 'swap'
-// })
+export const nunito = Nunito({
+	weight: ['500', '700'],
+	subsets: ['latin', 'cyrillic'],
+	variable: '--font-poppins',
+	display: 'swap'
+})

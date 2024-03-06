db03134c902e536b4ec4838cc0dc1e227ce2055d
Pavlo <tishkovets.pavlo@gmail.com>
2024-03-06 15:57:30 +0200

    added sorting

--- src/utils/filtersQueryBuilder.ts
+++ src/utils/filtersQueryBuilder.ts
@@ -1,44 +1,49 @@
 import { IFilter } from '@/types/types'
 
 export const filtersQueryBuilder = (
-    filters: IFilter[],
-    locale: string | string[]
+	filters: IFilter[],
+	locale: string | string[],
+	sorting: 'publishedAt' | 'price' | 'rating' = 'publishedAt',
+	sortingWay: 'asc' | 'desc' = 'desc'
 ): string => {
-    const baseurl = `http://localhost:1337/api/products?locale=${locale}&populate=images,properties,category,brand,product_comments`
+	const baseurl = `http://localhost:1337/api/products?locale=${locale}&populate=images,properties,category,brand,product_comments`
 
-    const filterQueries = filters.map((filter) => {
-        switch (filter.attributes.name) {
-            case 'price':
-                return `filters[price][$gt]=${filter.attributes.min_price}&filters[price][$lt]=${filter.attributes.max_price}`
-            case 'brand':
-                return filter.attributes.brands
-                    .filter((item) => item.active)
-                    .map(
-                        (item) => `filters[brand][name][$eq]=${item.brand.data.attributes.name}`
-                    )
-                    .join('&')
-            case 'category':    
-                return filter.attributes.categories
-                    .filter((item) => item.active)
-                    .map(
-                        (item) =>
-                            `filters[category][name][$eq]=${item.category.data.attributes.name}`
-                    )
-                    .join('&')
-            case 'stock':
-                return 'filters[stock][$gt]=0'
-            default:
-                return filter.attributes.options
-                    .filter((option) => option.active)
-                    .map(
-                        (option) =>
-                            `filters[properties][${filter.attributes.name}][$eq]=${option.title}`
-                    )
-                    .join('&')
-        }
-    })
+	const filterQueries = filters.map((filter) => {
+		switch (filter.attributes.name) {
+			case 'price':
+				return `filters[price][$gt]=${filter.attributes.min_price}&filters[price][$lt]=${filter.attributes.max_price}`
+			case 'brand':
+				return filter.attributes.brands
+					.filter((item) => item.active)
+					.map(
+						(item) =>
+							`filters[brand][name][$eq]=${item.brand.data.attributes.name}`
+					)
+					.join('&')
+			case 'category':
+				return filter.attributes.categories
+					.filter((item) => item.active)
+					.map(
+						(item) =>
+							`filters[category][name][$eq]=${item.category.data.attributes.name}`
+					)
+					.join('&')
+			case 'stock':
+				return 'filters[stock][$gt]=0'
+			default:
+				return filter.attributes.options
+					.filter((option) => option.active)
+					.map(
+						(option) =>
+							`filters[properties][${filter.attributes.name}][$eq]=${option.title}`
+					)
+					.join('&')
+		}
+	})
 
-    const validFilterQueries = filterQueries.filter(query => query !== ''); // Filter out empty queries
+	const validFilterQueries = filterQueries.filter((query) => query !== '') // Filter out empty queries
 
-    return `${baseurl}&${validFilterQueries.join('&')}`
+	const sortingFilter = `&sort=${sorting}:${sortingWay}`
+
+	return `${baseurl}&${validFilterQueries.join('&')}${sortingFilter}`
 }

24927f18d485d07148fec010015fe95aea66c842
Danil <danil.polovynkin.04@gmail.com>
2024-03-12 13:12:03 +0200

    completed translation of buttons

--- src/components/screens/Product/Tabs/Tabs.tsx
+++ src/components/screens/Product/Tabs/Tabs.tsx
@@ -2,20 +2,41 @@ import { FC } from 'react'
 import './Tabs.scss'
 import Button from '@/components/ui/buttons/Button/Button'
 import TabContainer from './TabContainer/TabContainer'
+import { useTranslations } from 'next-intl'
+
+const DescriptionTab: FC<{ description: string }> = ({ description }) => {
+	const t = useTranslations('product')
+
+	return (
+		<div className='description-tab'>
+			<h2 className='description__title'>{t('description')}</h2>
+
+			{description}
+		</div>
+	)
+}
 
 const Tabs: FC = () => {
+	const t = useTranslations('product')
+
 	return (
 		<div className='tabs'>
 			<div className='tabs__btns-wr'>
-				<Button className='tabs__btn active'>Описание</Button>
-				<Button className='tabs__btn'>Характеристики</Button>
-				<Button className='tabs__btn'>Аксессуары</Button>
-				<Button className='tabs__btn'>Отзывы</Button>
-				<Button className='tabs__btn'>Рассрочка и кредит</Button>
-				<Button className='tabs__btn active'>Наличие и доставка</Button>
+				<Button className='tabs__btn active'>{t('description')}</Button>
+				<Button className='tabs__btn'>{t('accessories')}</Button>
+				<Button className='tabs__btn'>{t('accessories')}</Button>
+				<Button className='tabs__btn'>{t('comments')}</Button>
+				<Button className='tabs__btn'>{t('credit')}</Button>
+				<Button className='tabs__btn active'>{t('deliver')}</Button>
 			</div>
 
-			<TabContainer children={<div>hhhhh</div>} />
+			<TabContainer
+				children={
+					<div>
+						<DescriptionTab description='xxx' />
+					</div>
+				}
+			/>
 		</div>
 	)
 }

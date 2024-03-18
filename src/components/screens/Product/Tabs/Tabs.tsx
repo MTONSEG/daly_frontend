2e86bfb96de6f9814ee2dba6bfb56b1263823210
Danil <danil.polovynkin.04@gmail.com>
2024-03-18 16:48:36 +0200

    refuctored a bit slider

--- src/components/screens/Product/Tabs/Tabs.tsx
+++ src/components/screens/Product/Tabs/Tabs.tsx
@@ -53,45 +53,52 @@ const Characteristics: FC<{ properities: IProductProperties }> = ({ properities 
 }
 
 const Credit = () => {
+	const t = useTranslations('product')
+
 	return (
 		<div className='credit'>
-			<h1 className='credit__title'>Рассрочка и кредит</h1>
+			<h1 className='credit__title'>{t('credit')}</h1>
 			<div className='credit-box'>
 				<h2 className='credit-box__title'>Как купить в рассрочку?</h2>
 				<ol className='credit-box__ol'>
 					<li className='credit-box__li'>
-						<span>1</span>.Добавьте товар в корзину
+						<span>1.</span>
+						{t('addToBusket')}
 					</li>
 					<li className='credit-box__li'>
-						<span>2.</span>Добавьте товар в корзину
+						<span>2.</span>
+						{t('addToBusket')}
 					</li>
 					<li className='credit-box__li'>
-						<span>3.</span>Добавьте товар в корзину
+						<span>3.</span>
+						{t('addToBusket')}
 					</li>
 					<li className='credit-box__li'>
-						<span>4.</span>Добавьте товар в корзину
+						<span>4.</span>
+						{t('addToBusket')}
 					</li>
 					<li className='credit-box__li'>
-						<span>5.</span>Добавьте товар в корзину
+						<span>5.</span>
+						{t('addToBusket')}
 					</li>
 				</ol>
 			</div>
 
 			<div className='credit-box credit-box_small'>
-				<h2 className='credit-box__title'>Как купить в рассрочку?</h2>
+				<h2 className='credit-box__title'>{t('mounthlyPayment')}</h2>
 				<ul className='credit-box__ol_installment '>
 					<li className='credit-box__li_installment'>
-						<p className='credix-box__left-text'>Ежемесячный платежу</p>
+						<p className='credix-box__left-text'>{t('mounthlyPayment')}</p>
 						<p className='credix-box__right-text'>8350 ∞ ₴</p>
 					</li>
 				</ul>
 			</div>
 
 			<div className='credit-box credit-box_small'>
-				<h2 className='credit-box__title'>Как купить в рассрочку?</h2>
+				<h2 className='credit-box__title'>{t('mounthlyPayment')}</h2>
 				<ul className='credit-box__ol_installment '>
 					<li className='credit-box__li_installment'>
-						<p className='credix-box__left-text'>Ежемесячный платежу</p>
+						<p className='credix-box__left-text'>{t('mounthlyPayment')}</p>
 						<p className='credix-box__right-text'>8350 ∞ ₴</p>
 					</li>
 				</ul>

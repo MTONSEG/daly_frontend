7e27ad8347263ea07066354ea9949520761845ad
Danil <danil.polovynkin.04@gmail.com>
2024-03-27 15:26:51 +0200

    added custom date changer

--- src/components/screens/Product/Tabs/Delivery/Delivery.tsx
+++ src/components/screens/Product/Tabs/Delivery/Delivery.tsx
@@ -1,27 +1,79 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import TabHead from '../TabStructure/TabHead'
 import TabContent from '../TabStructure/TabContent'
 import { ITab } from '../Tabs'
 import Button from '@/components/ui/buttons/Button/Button'
 import LinkBtn from '@/components/ui/buttons/LinkBtn/LinkBtn'
-import { PhoneIcon, TelIcon } from '@/components/ui/icons'
+import { TelIcon } from '@/components/ui/icons'
 import './Delivery.scss'
-import Select, { ActionMeta, OnChangeValue, StylesConfig } from 'react-select'
-import makeAnimated from 'react-select/animated'
+import Select, { StylesConfig } from 'react-select'
 import AsyncSelect from 'react-select/async'
 import { useGetAdressesMutation } from '../../../../../store/api/novaPost.api'
-import Option from 'react-select'
-import { IResponse } from '@/types/types'
 import { useTranslations } from 'next-intl'
-// import './Select.scss'
-
-// product-card
 
 const Courier = () => {
+	const generateDate = (numberOfDaysToAdd: number) => {
+		const currentDate = new Date()
+
+		const daysInMonth = (year: number, month: number) => new Date(year, month, 0).getDate()
+
+		if (currentDate.getDate() + numberOfDaysToAdd > daysInMonth(2024, currentDate.getMonth() + 1)) {
+			const difference =
+				currentDate.getDate() + numberOfDaysToAdd - daysInMonth(2024, currentDate.getMonth() + 1)
+			currentDate.setMonth(currentDate.getMonth() + 1)
+			currentDate.setDate(1 + difference)
+
+			return new Date(
+				`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate() - 1}`
+			).toLocaleString('default', {
+				day: 'numeric',
+				month: 'long'
+			})
+		} else {
+			return new Date(
+				`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${
+					currentDate.getDate() + numberOfDaysToAdd
+				}`
+			).toLocaleString('default', {
+				day: 'numeric',
+				month: 'long'
+			})
+		}
+	}
+
+	const workingHours = [
+		{ value: '10.00', label: '10.00 - 14.00' },
+		{ value: '14.00', label: '14.00 - 18.00' },
+		{ value: '18.00', label: '18.00 - 22.00' }
+	]
+
+	const availibleDate = [
+		{ value: 'today+1', label: generateDate(1) },
+		{
+			value: 'today+2',
+			label: generateDate(2)
+		},
+		{
+			value: 'today+3',
+			label: generateDate(3)
+		},
+		{
+			value: 'today+4',
+			label: generateDate(4)
+		},
+		{
+			value: 'today+5',
+			label: generateDate(5)
+		},
+		{
+			value: 'today+6',
+			label: generateDate(6)
+		}
+	]
+
 	const t = useTranslations('shared')
-	// const [city, setCity] = useState('Выберите город')
-	// const [date, setDate] = useState('')
-	// const [time, setTime] = useState('')
+	const tD = useTranslations('delivery')
+
 	const [adresses, setAdresses] = useState<{ value: string; label: string }[]>()
 	const [getAdresses] = useGetAdressesMutation()
 
@@ -61,22 +113,16 @@ const Courier = () => {
 		}
 	}
 
-	// const filterAdress = (inputValue: string) => {
-	// 	return adresses.filter((i) => i.DescriptionRu.toLowerCase().includes(inputValue.toLowerCase()))
-	// }
-
-	// const promiseOptions = (inputValue: string) =>
-	// 	new Promise((resolve) => {
-	// 		resolve(filterAdress(inputValue))
-	// 	})
-
 	const filterAdresses = (inputValue: string) => {
-		return (
-			adresses && adresses.filter((i) => i.label.toLowerCase().includes(inputValue.toLowerCase()))
-		)
+		return adresses
+			? adresses.filter((i) => i.label.toLowerCase().includes(inputValue.toLowerCase()))
+			: []
 	}
 
-	const loadOptions = (inputValue: string, callback: (options: any[]) => void) => {
+	const loadOptions = (
+		inputValue: string,
+		callback: (options: { label: string; value: string }[]) => void
+	) => {
 		callback(filterAdresses(inputValue))
 	}
 
@@ -85,7 +131,7 @@ const Courier = () => {
 			<div className='courier__left'>
 				<form id='courier-form' className='courier__form'>
 					<div className='select-container'>
-						<p className='select-label'>Город доставки</p>
+						<p className='select-label'>{tD('destinationCity')}</p>
 						<Select
 							required
 							onChange={(newValue) => {
@@ -98,18 +144,18 @@ const Courier = () => {
 							}}
 							styles={colourStyles}
 							className='select'
-							defaultValue={{ value: 'choose_city', label: 'choose city' }}
+							defaultValue={{ value: 'choose_city', label: tD('chooseCity') }}
 							options={[
-								{ value: 'харків', label: 'Харьков' },
-								{ value: 'дніпро', label: 'Днепр' },
-								{ value: 'київ', label: 'Киев' }
+								{ value: 'харків', label: tD('destinationCityKh') },
+								{ value: 'дніпро', label: tD('destinationCityDn') },
+								{ value: 'київ', label: tD('destinationCityKiev') }
 							]}
 						/>
 					</div>
 
 					<div className='selects-line'>
 						<div className='select-container'>
-							<p className='select-label'>Дата</p>
+							<p className='select-label'>{tD('date')}</p>
 							<Select
 								onChange={() => {
 									setIsChosen((prev) => {
@@ -117,19 +163,13 @@ const Courier = () => {
 									})
 								}}
 								styles={colourStyles}
-								defaultValue={{ value: 'Date', label: 'Дата' }}
-								options={[
-									{
-										value: 'x',
-										label: 'z'
-									}
-								]}
+								defaultValue={{ value: 'Date', label: tD('date') }}
+								options={availibleDate}
 							/>
-							{/* <MySelect value={date} setValue={setDate} valuesArr={['1', '2', '3']}></MySelect> */}
 						</div>
 
 						<div className='select-container'>
-							<p className='select-label'>Время</p>
+							<p className='select-label'>{tD('time')}</p>
 							<Select
 								onChange={() => {
 									setIsChosen((prev) => {
@@ -137,28 +177,26 @@ const Courier = () => {
 									})
 								}}
 								styles={colourStyles}
-								defaultValue={{ value: 'Time', label: 'Время' }}
-								options={[
-									{
-										value: '10-14',
-										label: '10-14'
-									}
-								]}
+								defaultValue={{ value: 'Time', label: tD('time') }}
+								options={workingHours}
 							/>
 						</div>
 					</div>
 
 					<div className='select-container'>
-						<p className='select-label'>Адресс</p>
+						<p className='select-label'>{tD('adress')}</p>
 						<AsyncSelect
-							// loadOptions={promiseOptions}
-							// onChange={onCityChangeHandler}
-							// isDisabled={isChoosen.city && isChoosen.date && isChoosen.time ? false : true}
+							isDisabled={isChoosen.city && isChoosen.date && isChoosen.time ? false : true}
+							onChange={() =>
+								setIsChosen((prev) => {
+									return { ...prev, adress: true }
+								})
+							}
 							placeholder={t('search-placeholder')}
 							loadingMessage={() => t('searching')}
 							noOptionsMessage={() => t('no-searched')}
 							styles={colourStyles}
-							defaultValue={{ value: 'Adress', label: 'Выберете Адрес' }}
+							defaultValue={{ value: 'Adress', label: tD('adress') }}
 							loadOptions={loadOptions}
 							defaultOptions={adresses}
 						/>
@@ -169,12 +207,16 @@ const Courier = () => {
 							isChoosen.adress && isChoosen.city && isChoosen.date && isChoosen.time ? false : true
 						}
 						variant='product'
+						type='submit'
+						onClick={(e) => {
+							e.preventDefault()
+						}}
 					>
-						Купить
+						{tD('buyBtn')}
 					</Button>
 				</form>
 				<LinkBtn href='' className='courier__link'>
-					Полные условия доставки
+					{tD('linkText')}
 				</LinkBtn>
 			</div>
 		</div>

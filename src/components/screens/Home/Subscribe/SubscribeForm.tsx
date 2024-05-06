925f6cde9fb30f2955c3c9499c1c4ef3719136d8
Maksym Korostelov <flying1519@gmail.com>
2024-05-06 21:05:29 +0300

    subscribe form add button

--- src/components/screens/Home/Subscribe/SubscribeForm.tsx
+++ src/components/screens/Home/Subscribe/SubscribeForm.tsx
@@ -2,26 +2,30 @@ import '../Home.scss'
 import { useForm } from 'react-hook-form'
 import { TextField } from '@mui/material'
 import { ISubscribe } from '@/types/types'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { subscribeApi } from '@/store/api/subscribe.api'
-
+import Button from '@/components/ui/buttons/Button/Button'
 
 const SubscribeForm = () => {
-//useForm-------------------------------------------------
-const form = useForm<ISubscribe>({})
-const { register, handleSubmit, formState, reset } = form
-const { errors } = formState
+	//useForm-------------------------------------------------
+	const form = useForm<ISubscribe>({})
+	const { register, handleSubmit, formState, reset } = form
+	const { errors } = formState
 
-useEffect(() => {
-	reset()
-}, [])
+	useEffect(() => {
+		reset()
+	}, [])
+
+	const [choosed, setChoosed] = useState<boolean>(false)
+
 	return (
-			<form onSubmit={handleSubmit(subscribeApi())}>
+		<form onSubmit={handleSubmit(subscribeApi())} className='subscribe-form'>
+			<div className='subscribe-form__inputs'>
 				<TextField
 					id='standard-basic'
 					placeholder='Ваш e-mail'
-					variant='standard'
-					style={{ minWidth: '100%'}}
+					variant='outlined'
+					style={{ minWidth: '100%' }}
 					type='text'
 					{...register('data.subscriber', {
 						required: 'Email is required'
@@ -29,10 +33,24 @@ useEffect(() => {
 					error={!!errors.data?.subscriber}
 					helperText={errors.data?.subscriber?.message}
 				/>
-
-				<button type='submit'>click</button>
-			</form>
-)
+				<div className='subscribe-form__terms'>
+					<div
+						className={
+							choosed ? 'subscribe-form__terms-mark-active' : 'subscribe-form__terms-mark-nonactive'
+						}
+						onClick={() => setChoosed(!choosed)}
+					><div></div></div>
+					<div className='subscribe-form__terms-text'>
+						Я согласен с условиями обработки <span>персональных данных</span>, а также с{' '}
+						<span>условиями подписки</span>
+					</div>
+				</div>
+			</div>
+			<div>
+			<button className='subscribe-form__button'>Подписаться</button>
+			</div>
+		</form>
+	)
 }
 
 export default SubscribeForm

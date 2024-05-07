2bb4482374020208f1ef03b803937f29b39ced48
Maksym Korostelov <flying1519@gmail.com>
2024-05-07 20:57:46 +0300

    adaptive brands completed/mui deleted

--- src/components/screens/Home/Subscribe/SubscribeForm.tsx
+++ src/components/screens/Home/Subscribe/SubscribeForm.tsx
@@ -1,9 +1,10 @@
 import '../Home.scss'
 import { useForm } from 'react-hook-form'
-import { TextField } from '@mui/material'
 import { ISubscribe } from '@/types/types'
 import { useState, useEffect } from 'react'
 import { subscribeApi } from '@/store/api/subscribe.api'
+import Checkbox from '@/components/ui/checkboxes/Checkbox'
+import Input from '@/components/ui/forms/Input/Input'
 import { useTranslations } from 'next-intl'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -32,27 +33,9 @@ const SubscribeForm = () => {
 	return (
 		<form onSubmit={onSubmit} className='subscribe-form'>
 			<div className='subscribe-form__inputs'>
-				<TextField
-					id='standard-basic'
-					placeholder={word("placeholder")}
-					variant='outlined'
-					style={{ minWidth: '100%' }}
-					type='text'
-					{...register('data.subscriber', {
-						required: 'Email is required'
-					})}
-					error={!!errors.data?.subscriber}
-					helperText={errors.data?.subscriber?.message}
-				/>
+				<Input type='email' placeholder={word("placeholder")}/>
 				<div className='subscribe-form__terms'>
-					<div
-						className={
-							choosed ? 'subscribe-form__terms-mark-active' : 'subscribe-form__terms-mark-nonactive'
-						}
-						onClick={() => setChoosed(!choosed)}
-					>
-						<div></div>
-					</div>
+					<Checkbox label="" isActive={choosed} toggleCheckbox={() => setChoosed(!choosed)}/>
 					<div className='subscribe-form__terms-text'>
 						{word("agreement-text1")} <span>{word("agreement-text2")} </span>, {word("agreement-text3")}{' '}
 						<span>{word("agreement-text4")}</span>

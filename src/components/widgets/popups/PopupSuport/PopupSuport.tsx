65936d820ee15ed4495eba662ae96c9882023caf
Maksym Korostelov <flying1519@gmail.com>
2024-05-20 17:00:04 +0300

    favorites data setUp in children PopupHeaderContainer

--- src/components/widgets/popups/PopupSuport/PopupSuport.tsx
+++ src/components/widgets/popups/PopupSuport/PopupSuport.tsx
@@ -2,7 +2,7 @@
 import './PopupSuport.scss'
 import { useAppDispatch } from '@/hooks/useReduxHooks'
 import { setSuccessForm } from '@/store/popups/supportPopup.slice'
-import { useEffect } from 'react'
+import { useEffect, forwardRef } from 'react'
 import { ISuport } from '@/types/types'
 import { useForm } from 'react-hook-form'
 import SuportForm from './SuportForm'
@@ -10,51 +10,51 @@ import { suportApi } from '@/store/api/support.api'
 import PopupSuportBottom from './PopupSuportBottom'
 import Image from 'next/image'
 import GreenCross from '@/images/green-cross.webp'
-import { truncate } from 'fs'
+
 
 interface PopupTypes {
-	closePopup: () => void
+    closePopup: () => void
 }
 
-const PopupSuport = ({ closePopup }: PopupTypes) => {
-	//useForm-------------------------------------------------
-	const form = useForm<ISuport>({})
-	const { register, handleSubmit, formState, reset } = form
-	const { errors } = formState
+const PopupSuport = forwardRef<HTMLDivElement, PopupTypes>(({ closePopup }, ref) => {
+    // useForm-------------------------------------------------
+    const form = useForm<ISuport>({})
+    const { register, handleSubmit, formState, reset } = form
+    const { errors } = formState
 
-	const onSubmit = handleSubmit(suportApi(reset))
+    const onSubmit = handleSubmit(suportApi(reset))
 
-	const handleImage = (e: File | null) => {
-		form.setValue('data.image', e)
-	}
-	//-----------------------------------------------------
-	const dispatch = useAppDispatch()
-	useEffect(() => {
-		if (formState.isSubmitSuccessful) {
-			dispatch(setSuccessForm(true))
-		}
-	}, [formState.isSubmitSuccessful])
+    const handleImage = (e: File | null) => {
+        form.setValue('data.image', e)
+    }
+    //-----------------------------------------------------
+    const dispatch = useAppDispatch()
+    useEffect(() => {
+        if (formState.isSubmitSuccessful) {
+            dispatch(setSuccessForm(true))
+        }
+    }, [formState.isSubmitSuccessful, dispatch])
 
-	return (
-		<form onSubmit={onSubmit}>
-			<div className='popup-suport'>
-				<div
-					style={{
-						position: 'relative',
-						minWidth: '100%',
-						minHeight: '100%',
-					}}
-				>
-					<h2 className='popup-suport__title'>Обращение в службу поддержки</h2>
-					<SuportForm register={register} errors={errors} />
-					<PopupSuportBottom register={register} handleImage={handleImage} />
-					<div className='popup-suport__close-wrapper' onClick={closePopup}>
-						<Image src={GreenCross} width={30} height={30} alt='cross' />
-					</div>
-				</div>
-			</div>
-		</form>
-	)
-}
+    return (
+        <form onSubmit={onSubmit}>
+            <div className='popup-suport' ref={ref}>
+                <div
+                    style={{
+                        position: 'relative',
+                        minWidth: '100%',
+                        minHeight: '100%',
+                    }}
+                >
+                    <h2 className='popup-suport__title'>Обращение в службу поддержки</h2>
+                    <SuportForm register={register} errors={errors} />
+                    <PopupSuportBottom register={register} handleImage={handleImage} />
+                    <div className='popup-suport__close-wrapper' onClick={closePopup}>
+                        <Image src={GreenCross} width={30} height={30} alt='cross' />
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
+})
 
 export default PopupSuport

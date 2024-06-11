f5b6c298d61fd8ae983a1aad6d60e56dbe68c2c6
Pavlo <tishkovets.pavlo@gmail.com>
2024-06-11 22:28:39 +0300

    added wrappers and active classes

--- src/components/widgets/SliderThumbnail/SliderThumbnailFancyApp.tsx
+++ src/components/widgets/SliderThumbnail/SliderThumbnailFancyApp.tsx
@@ -9,60 +9,76 @@ import 'swiper/css/pagination'
 import 'swiper/css/controller'
 import SwiperCore from 'swiper'
 import { Controller } from 'swiper/modules'
+import useOutsideClick from '@/hooks/useOutSideClick'
 
 // Initialize Swiper modules
 SwiperCore.use([Controller])
 const SliderThumbnailFancyApp: FC<{ images: IProductImage[] }> = ({ images }) => {
 	const [mainSwiper, setMainSwiper] = useState<SwiperCore | null>(null)
-	console.log('🚀 ~ mainSwiper:', mainSwiper?.activeIndex)
 	const [thumbSwiper, setThumbSwiper] = useState<SwiperCore | null>(null)
-	console.log('🚀 ~ thumbSwiper:', thumbSwiper?.activeIndex)
+	const { ref, isActive, setIsActive } = useOutsideClick<HTMLDivElement>(false)
 
 	return (
 		<div className='product-slider'>
-			<Swiper
-				className='product-slider__main-slider'
-				slidesPerView={1}
-				spaceBetween={25}
-				initialSlide={0}
-				slideToClickedSlide={true}
-				onSwiper={setMainSwiper} // Set the main swiper instance
-				controller={{ control: thumbSwiper }} // Sync with thumbnail swiper
-			>
-				{images.map((el, key) => (
-					<SwiperSlide key={key} className='product-slider__main-slide'>
-						<div className='product-slider__main-img-box'>
-							<Image
-								fill={true}
-								src={el.url}
-								alt='slider-big-img'
-								className='product-slider__main-img'
-							/>
-						</div>
-					</SwiperSlide>
-				))}
-			</Swiper>
-			<Swiper
-				className='product-slider__thumbnail-slider'
-				slidesPerView={3}
-				spaceBetween={25}
-				initialSlide={0}
-				centeredSlides={true}
-				onSwiper={setThumbSwiper} // Set the thumbnail swiper instance
-				slideToClickedSlide={true}
-				controller={{ control: mainSwiper }} // Sync with main swiper
-			>
-				{images.map((el, key) => (
-					<SwiperSlide key={key} className='product-slider__thumbnail-slide'>
-						<Image
-							fill={true}
-							src={el.url}
-							alt='slider-big-img'
-							className='product-slider__thumbnail-img'
-						/>
-					</SwiperSlide>
-				))}
-			</Swiper>
+			<div className='product-slider__slider-placeholder'></div>
+			<div className={`product-slider__popup-wrapper ${isActive && 'active'}`}>
+				<div className={`product-slider__popup-container ${isActive && 'active'}`} ref={ref}>
+					<Swiper
+						className={`product-slider__main-slider ${isActive && 'active'}`}
+						slidesPerView={1}
+						spaceBetween={25}
+						initialSlide={0}
+						slideToClickedSlide={true}
+						onSwiper={setMainSwiper} // Set the main swiper instance
+						controller={{ control: thumbSwiper }} // Sync with thumbnail swiper
+					>
+						{images.map((el, key) => (
+							<SwiperSlide
+								key={key}
+								className={`product-slider__main-slide ${isActive && 'active'}`}
+							>
+								<div
+									className='product-slider__main-img-box'
+									onClick={() => {
+										setIsActive(true)
+									}}
+								>
+									<Image
+										fill={true}
+										src={el.url}
+										alt='slider-big-img'
+										className='product-slider__main-img'
+									/>
+								</div>
+							</SwiperSlide>
+						))}
+					</Swiper>
+					<Swiper
+						className={`product-slider__thumbnail-slider ${isActive && 'active'}`}
+						slidesPerView={3}
+						spaceBetween={25}
+						initialSlide={0}
+						centeredSlides={true}
+						onSwiper={setThumbSwiper} // Set the thumbnail swiper instance
+						slideToClickedSlide={true}
+						controller={{ control: mainSwiper }} // Sync with main swiper
+					>
+						{images.map((el, key) => (
+							<SwiperSlide
+								key={key}
+								className={`product-slider__thumbnail-slide ${isActive && 'active'}`}
+							>
+								<Image
+									fill={true}
+									src={el.url}
+									alt='slider-big-img'
+									className='product-slider__thumbnail-img'
+								/>
+							</SwiperSlide>
+						))}
+					</Swiper>
+				</div>
+			</div>
 		</div>
 	)
 }

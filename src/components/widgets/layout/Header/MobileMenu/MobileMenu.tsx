143a80992ca56d9ee7f105f42a1ff656b84bf616
Maksym Korostelov <flying1519@gmail.com>
2024-06-18 10:12:56 +0300

    buttons register fixed 10:12

--- src/components/widgets/layout/Header/MobileMenu/MobileMenu.tsx
+++ src/components/widgets/layout/Header/MobileMenu/MobileMenu.tsx
@@ -1,7 +1,7 @@
 import './MobileMenu.scss'
 import { useEffect } from 'react'
 import LocaleSelect from '@/components/ui/forms/LocaleSelect/LocaleSelect'
-import LinkBtn from '@/components/ui/Buttons/LinkBtn/LinkBtn'
+import LinkBtn from '@/components/ui/buttons/LinkBtn/LinkBtn'
 import Logo from '@/components/ui/icons/Logo/Logo'
 import { UserIcon } from '@/components/ui/icons'
 import { useTranslations } from 'next-intl'
